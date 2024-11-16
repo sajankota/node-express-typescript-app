d2a6fe2a8a7ffd8ed1c521d7ea188b472cd9a9c8
Sajan Kota <sajan@steinnlabs.com>
2024-11-17 03:47:55 +0530

    updated the index.ts to separate the mongodb connection

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,10 +3,8 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
-        const mongoURI =
-            process.env.NODE_ENV === 'production'
-                ? process.env.MONGO_URI_PROD
-                : process.env.MONGO_URI_LOCAL;
+        // Use a single environment variable for MongoDB URI
+        const mongoURI = process.env.MONGO_URI;
 
         if (!mongoURI) {
             throw new Error('MongoDB URI is not defined in the environment variables.');
@@ -15,7 +13,7 @@ const connectDB = async () => {
         // Connect to MongoDB without deprecated options
         await mongoose.connect(mongoURI);
 
-        console.log(`Connected to MongoDB (${process.env.NODE_ENV})`);
+        console.log('Connected to Database');
     } catch (error) {
         console.error('MongoDB connection error:', error);
         process.exit(1); // Exit the process with failure
