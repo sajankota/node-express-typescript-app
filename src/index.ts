9bf827b915fa535533668fbb25c3de61c9e74446
Sajan Kota <sajan@steinnlabs.com>
2024-11-16 18:37:35 +0530

    added cors and .gitignore

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+
+// Enable CORS for all routes
+app.use(cors());
+app.use(
+    cors({
+      origin: ['https://wwwroundcodebox.com', 'http://localhost:5173'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+    })
+  );
+  
 
 // Simple endpoint for testing
 app.get('/', (req: Request, res: Response) => {
@@ -10,5 +22,5 @@ app.get('/', (req: Request, res: Response) => {
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
