9a4a6377579564d5234f47540133d687c006f067
Sajan Kota <sajan@steinnlabs.com>
2024-11-16 20:24:01 +0530

    added sample endpoint

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,11 @@ const PORT = process.env.PORT || 4000;
 // Enable CORS
 app.use(cors());
 
+// Simple endpoint for testing
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello from Node Express TypeScript app!');
+});
+
 // Health check endpoint
 app.get('/api/health-check', (req: Request, res: Response) => {
   res.json({ message: 'Hello from Node.js API!' });
