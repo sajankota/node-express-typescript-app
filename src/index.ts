7c737aa247918d29e3d6cb6e3df4afe576d7d759
Sajan Kota <sajan@steinnlabs.com>
2024-11-16 19:59:59 +0530

    updated the index.ts file to a new endpoint

--- src/index.ts
+++ src/index.ts
@@ -4,23 +4,15 @@ import cors from 'cors';
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-// Enable CORS for all routes
+// Enable CORS
 app.use(cors());
-app.use(
-    cors({
-      origin: ['https://wwwroundcodebox.com', 'http://localhost:5173'],
-      methods: ['GET', 'POST', 'PUT', 'DELETE'],
-      allowedHeaders: ['Content-Type', 'Authorization'],
-    })
-  );
-  
 
-// Simple endpoint for testing
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello from Node Express TypeScript app!');
+// Health check endpoint
+app.get('/api/health-check', (req: Request, res: Response) => {
+  res.json({ message: 'Hello from Node.js API!' });
 });
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on http://0.0.0.0:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
