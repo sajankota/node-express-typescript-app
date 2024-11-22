19cbe20b2605678e8b568c959deb36dc2876c617
Sajan Kota <sajan@steinnlabs.com>
2024-11-22 22:56:43 +0530

    implemented SEO metrics for mobiel and desktop

--- src/routes/seoMetricsRoutes.ts
+++ src/routes/seoMetricsRoutes.ts
@@ -2,11 +2,14 @@
 
 import express from "express";
 import { authMiddleware } from "../middleware/authMiddleware";
-import { getSEOMetrics } from "../controllers/seoMetricsController";
+import { getMobileSEOMetrics, getDesktopSEOMetrics } from "../controllers/seoMetricsController";
 
 const router = express.Router();
 
 // Route to fetch SEO metrics by report ID
-router.get("/:reportId", authMiddleware, getSEOMetrics); // Fetch a specific SEO metric by report ID
+router.get("/mobile/:reportId", authMiddleware, getMobileSEOMetrics); // Fetch a specific SEO metric by report ID
+
+// Route to fetch SEO metrics for desktop by report ID
+router.get("/desktop/:reportId", authMiddleware, getDesktopSEOMetrics);
 
 export default router;
